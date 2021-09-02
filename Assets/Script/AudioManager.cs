55bff6e18cc95d2b48835c0a8d776d9fb9fb4913
Rokr0k <yangeh2225@gmail.com>
2021-09-02 21:28:54 +0900

    implement basic note

---
+++ Assets/Script/AudioManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    private static double audioTime;
+    private static double prevTime;
+    private static double addTime;
+    public static double AudioTime
+    {
+        get
+        {
+            return AudioSettings.dspTime + addTime - audioTime;
+        }
+    }
+
+    private AudioSource audioSource;
+
+    void Awake()
+    {
+        audioTime = AudioSettings.dspTime + 2;
+        prevTime = AudioSettings.dspTime;
+        addTime = 0;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.PlayScheduled(audioTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (AudioSettings.dspTime == prevTime)
+            addTime += Time.unscaledDeltaTime;
+        else
+            addTime = 0;
+        prevTime = AudioSettings.dspTime;
+    }
+}
